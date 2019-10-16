f3700944c2595905f7c63e7cbce00cf81ae06f18
adriano.ramazzotte <adriano.ramazzotte@gmail.com>
2019-10-16 10:38:19 -0300

    Primeira Versão

---
+++ src/main/java/com/ramazzotte/estudoBase/EstudoBaseApplication.java
@@ -0,0 +1,13 @@
+package com.ramazzotte.estudoBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoBaseApplication.class, args);
+	}
+
+}
