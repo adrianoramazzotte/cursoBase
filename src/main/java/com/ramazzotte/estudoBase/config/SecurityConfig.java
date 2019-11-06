703f7adc666ffc42c7186dde1658ff4003f0c055
adrianoramazzotte <adriano.ramazzotte@gmail.com>
2019-11-06 17:22:24 -0300

    Implementando autorizacao

--- src/main/java/com/ramazzotte/estudoBase/config/SecurityConfig.java
+++ src/main/java/com/ramazzotte/estudoBase/config/SecurityConfig.java
@@ -19,20 +19,26 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ramazzotte.estudoBase.security.JWTAuthenticationFilter;
+import com.ramazzotte.estudoBase.security.JWTAuthorizationFilter;
 import com.ramazzotte.estudoBase.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	@Autowired
-	private JWTUtil  jwtUtil;	
-	@Autowired
-	private Environment env;
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+    private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
 	};
+	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
@@ -40,20 +46,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	};
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
+		
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
-		}
+            http.headers().frameOptions().disable();
+        }
+		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-				.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
-				.antMatchers(PUBLIC_MATCHERS).permitAll()
-				.anyRequest().authenticated();
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+
 	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
@@ -63,6 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
